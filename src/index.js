15835a615ff435b1231003d5b9a47d6930b7773a
GucciGen <sviaticrrocer@gmail.com>
2025-11-21 14:28:03 +0200

    Initial TypeScript project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Sviatoslav";
+let age = 20;
+let isStudent = true;
+console.log('Name: ${username}');
+console.log('Age: ${age}');
+console.log('Is Student: ${isStudent}');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let username: string = "Sviatoslav";
+let age: number = 20;
+let isStudent: boolean = true;
+
+console.log('Name: ${username}');
+console.log('Age: ${age}');
+console.log('Is Student: ${isStudent}');
