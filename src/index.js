58230637a6eeee168df0afdfc0a5ade679740c4d
GucciGen <77923692+GucciGen@users.noreply.github.com>
2025-12-04 15:13:49 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let username = "Sviatoslav";
-let age = 20;
-let isStudent = true;
-console.log('Name: ${username}');
-console.log('Age: ${age}');
-console.log('Is Student: ${isStudent}');
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-let username: string = "Sviatoslav";
-let age: number = 20;
-let isStudent: boolean = true;
-
-console.log('Name: ${username}');
-console.log('Age: ${age}');
-console.log('Is Student: ${isStudent}');
